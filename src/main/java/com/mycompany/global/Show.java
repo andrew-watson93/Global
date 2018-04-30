bf6f5b520fbd93ce0949ad4e369b73acddcbe14e
andrew-watson93 <andrewjw64@gmail.com>
2018-04-30 20:22:59 +0100

    intial commit

---
+++ src/main/java/com/mycompany/global/Show.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.global;
+
+/**
+ *
+ * @author andre
+ */
+public class Show {
+
+    private String from;
+    private String to;
+    private String longName;
+    private String synopsis;
+
+    /**
+     * @return the from
+     */
+    public String getFrom() {
+        return from;
+    }
+
+    /**
+     * @param from the from to set
+     */
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    /**
+     * @return the to
+     */
+    public String getTo() {
+        return to;
+    }
+
+    /**
+     * @param to the to to set
+     */
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    /**
+     * @return the longName
+     */
+    public String getLongName() {
+        return longName;
+    }
+
+    /**
+     * @param longName the longName to set
+     */
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    /**
+     * @return the synopsis
+     */
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    /**
+     * @param synopsis the synopsis to set
+     */
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+}
